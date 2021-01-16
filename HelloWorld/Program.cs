77e3e0a4fdff3e83021ca43c13e4be74a295dea6
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-01-16 10:44:06 -0600

    all exercises complete except last 3 challenges on validator

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,65 +7,188 @@ namespace HelloWorld
         static void Main(string[] args)
         {
 
-            // Validator
+            // ***** Validator *****
 
+            // *** Validate a pin number ***
+
+            /*
             Console.WriteLine("Enter a pin:");
             var pinInput = Console.ReadLine();
 
-            string result = "";
+            string pinResult = "";
 
             char[] pinChars = pinInput.ToCharArray();
 
-            if (pinChars.Length > 4 || pinChars.Length < 8)
+            if (pinChars.Length >= 4 && pinChars.Length <= 8)
             {
-                foreach (var char in pinChars)
-                result = "true";
-            } else
-            {
-                result = "false";
+                foreach (var character in pinChars)
+                {
+                    if (Char.IsNumber(character))
+                    {
+                        pinResult = "true";
+                    }
+                    else
+                    {
+                        pinResult = "false";
+                        break;
+                    }
+                }
             }
-            
-            // Mask Sensitive Information
+            else
+            {
+                pinResult = "false";
+            }
 
-            Console.WriteLine("Tell me a secret:");
-            var secretInput = Console.ReadLine();
+            Console.WriteLine(pinResult);
+            */
 
+            // *** Validate a phone number ***
+
+            /*
+            Console.WriteLine("Enter a phone number:");
+            var phoneInput = Console.ReadLine();
+
+            var charsToRemove = new string[] { " ", "-", "(", ")" };
+
+            var phoneResult = "";
+
+            foreach (var c in charsToRemove)
+            {
+                phoneInput = phoneInput.Replace(c, string.Empty);
+            }
+
+            char[] phoneChars = phoneInput.ToCharArray();
+
+            if (phoneChars[0] == '5' && phoneChars[1] == '5' && phoneChars[2] == '5')
+            {
+                phoneResult = "Please enter a valid phone number.";
+            }
+            else if (phoneChars.Length == 10)
+            {
+                foreach (var c in phoneChars)
+                {
+                    if (Char.IsNumber(c))
+                    {
+                        phoneResult = "true";
+                    }
+                    else
+                    {
+                        phoneResult = "Please enter a valid phone number.";
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                phoneResult = "Please enter a valid phone number.";
+            }
+
+            Console.WriteLine(phoneResult);
+            */
+
+            // *** Validate an email address ***
+
+            /*
+            Console.WriteLine("Please enter your email address:");
+            var emailInput = Console.ReadLine();
+
+            var emailResponse = "Please enter a valid email address.";
+
+            char[] emailChars = emailInput.ToCharArray();
+
+            var atCounter = 0;
+            var dotCounter = 0;
+            var indexOfAt = Array.IndexOf(emailChars, '@');
+            var indexOfDot = 0;
+
+            var currentIndex = 0;
+
+            foreach (var character in emailChars)
+            {
+                if (character == '@')
+                {
+                    atCounter++;
+                }
+                else if (character == '.' && currentIndex > indexOfAt)
+                {
+                    dotCounter++;
+                    indexOfDot = currentIndex;
+                }
+                currentIndex++;
+            }
+
+            var domain = emailInput.Substring(indexOfDot);
+            bool tldPresent = false;
+            string[] tlds = { ".com", ".org", ".net", ".us", ".co", ".uk", ".de", ".tk", ".cn" };
+
+            foreach (var tld in tlds)
+            {
+                if (tld == domain)
+                {
+                    tldPresent = true;
+                    break;
+                }
+            }
+
+            if (atCounter == 1 && dotCounter == 1 && emailChars[0] != '@' && emailChars[indexOfAt + 1] != '.' && tldPresent)
+            {
+                if (indexOfAt < indexOfDot)
+                {
+                    emailResponse = "true";
+                }
+            }
+
+            Console.WriteLine(emailResponse);
+            */
+
+
+            // ***** Mask Sensitive Information *****
+
+            //Console.WriteLine("Tell me a secret:");
+            //var secretInput = Console.ReadLine();
+
+            //var maskedSecret = secretInput.Substring(secretInput.Length - 4).PadLeft(secretInput.Length, '*');
+
+            /*
             char[] secretChars = secretInput.ToCharArray();
 
             var secretOutput = "";
 
             for (var i = 0; i < secretChars.Length; i++)
             {
-                if (i < secretChars.Length - 4)
+                if (i > secretChars.Length - 4)
                 {
                     secretOutput += secretChars[i];
-                } else
+                }
+                else
                 {
                     secretOutput += "*";
                 }
             }
+            */
 
-            Console.WriteLine(secretOutput);
+            //Console.WriteLine(maskedSecret);
 
-            // Calculator Exercise
 
-            // Multiplication or Squares
+            // ***** Calculator Exercise *****
 
-            Console.WriteLine("Enter a list of numbers, with each number separated by a comma.");
-            var numbersInput = Console.ReadLine();
+            // *** Multiplication or Squares ***
 
-            Console.WriteLine("Would you like to square these numbers or multiply them? Type 'square' or 'multiply'.");
-            var operationInput = Console.ReadLine();
+            
+            Console.WriteLine("Enter an operation and a space, followed by a list of numbers, with each number separated by a comma.");
+            var calculationInput = Console.ReadLine();
 
-            string[] splitArray = numbersInput.Split(",");
+            var indexOfSpace = calculationInput.IndexOf(' ');
+
+            string numbersString = calculationInput.Substring(indexOfSpace + 1);
+            string operationString = calculationInput.Substring(0, indexOfSpace);
+
+            string[] splitArray = numbersString.Split(",");
             int[] intArray = Array.ConvertAll(splitArray, int.Parse);
 
-            int product = 1;
-
-            string squaredResult = "";
-
-            if (operationInput == "square")
+            if (operationString == "^2")
             {
+                string squaredResult = "";
                 for (var i = 0; i < intArray.Length; i++)
                 {
                     if (i == intArray.Length - 1)
@@ -78,20 +201,51 @@ namespace HelloWorld
                         squaredResult += $"{result},";
                     }
                 }
-                Console.WriteLine($"{squaredResult}");
+                Console.WriteLine(squaredResult);
             }
-            else if (operationInput == "multiply")
+            else if (operationString == "*")
             {
+                int product = 1;
                 foreach (var number in intArray)
                 {
                     product *= number;
                 }
 
-                Console.WriteLine($"{product}");
+                Console.WriteLine(product);
+            }
+            else if (operationString == "+")
+            {
+                int sum = 0;
+                foreach (var number in intArray)
+                {
+                    sum += number;
+                }
+                Console.WriteLine(sum);
+            }
+            else if (operationString == "/")
+            {
+                var quotient = intArray[0];
+                for (var i = 1; i < intArray.Length; i++)
+                {
+                    quotient /= intArray[i];
+                }
+                Console.WriteLine(quotient);
+            } else if (operationString == "avg")
+            {
+                int sum = 0;
+                foreach (var number in intArray)
+                {
+                    sum += number;
+                }
+                int average = sum / intArray.Length;
+                Console.WriteLine(average);
             }
 
-            // Letter Loops
 
+
+            // ***** Letter Loops *****
+
+            /*
             Console.WriteLine("Enter a string of characters:");
             var input = Console.ReadLine();
 
@@ -123,8 +277,11 @@ namespace HelloWorld
             }
 
             Console.WriteLine($"{output}");
+            */
 
-            // Console Input Exercise
+
+
+            // ***** Console Input Exercise *****
 
             /*
             ConsoleKeyInfo enteredKey;
@@ -147,7 +304,9 @@ namespace HelloWorld
             } while (sentence != "quit");
             */
 
-            // Hello, World Exercise
+
+
+            // ***** Hello, World Exercise *****
 
             /*
             Console.WriteLine("Howdy Y'all!!");
